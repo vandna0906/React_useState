223829e5b2dfd543c47e380c9d09b95acd3b75b2
Vandna <vandana100718@gmail.com>
2021-12-02 16:52:10 +0530

    todo_delete

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import "./App.css";
 //import { Counter }  from "./Counter";
 //import { Todo } from "./Components/Todo";
 //import CountDown from "./Components/CountDown/CountDown";
-import {ListKey} from "./Components/pagination/listKey"
+
+//import {ListKey} from "./Components/pagination/listKey"
+import InputTaking from "./Components/axios_practice/InputTaking"
 function App(){
     
 return (
     <div className="App">
+        <InputTaking/>
     {/* <Counter/>
      <Todo/> */}
-     {/* <CountDown/> */}
-     <ListKey/>
+     {/* <CountDown/>
+     <ListKey/> */}
     </div>
 )
    
